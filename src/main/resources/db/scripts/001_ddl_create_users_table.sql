3125f88b7d95312c8c037531542762579ef076b6
GANZO9055 <kosta.maslihov@gmail.com>
2025-11-23 23:30:51 +0300

    refactor: update sql query

--- src/main/resources/db/scripts/001_ddl_create_users_table.sql
+++ src/main/resources/db/scripts/001_ddl_create_users_table.sql
@@ -1,6 +1,6 @@
 create table users(
     id serial primary key,
     username varchar(255) not null unique,
-    password varchar not null,
-    role
+    password varchar(255) not null,
+    role varchar(255) not null
 );
