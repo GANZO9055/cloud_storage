5e3720c0e59f311eee88d613847c302843ceddb2
GANZO9055 <kosta.maslihov@gmail.com>
2025-12-07 14:55:15 +0300

    feat: add validation User

--- src/main/java/com/example/cloud_storage/user/model/User.java
+++ src/main/java/com/example/cloud_storage/user/model/User.java
@@ -1,6 +1,8 @@
 package com.example.cloud_storage.user.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,8 +19,21 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Username cannot be empty!")
+    @Size(
+            min = 3,
+            max = 50,
+            message = "Length of the user must be from 3 to 50 characters!"
+    )
     private String username;
+
+    @NotBlank(message = "Password cannot be empty!")
+    @Size(
+            min = 4,
+            max = 100,
+            message = "Length of the password must be from 3 to 100 characters!"
+    )
     private String password;
-    private String role;
+    private Role role;
 }
 
