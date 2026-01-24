dbc629b57a494f5761a9d7ae8d1e193be46f2283
GANZO9055 <kosta.maslihov@gmail.com>
2026-01-24 22:36:53 +0300

    fix(auth): add request matchers for interface

--- src/main/java/com/example/cloud_storage/user/config/SecurityConfig.java
+++ src/main/java/com/example/cloud_storage/user/config/SecurityConfig.java
@@ -29,6 +29,7 @@ public class SecurityConfig {
                         auth -> auth
                                         .requestMatchers("/api/auth/**").permitAll()
                                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                                        .requestMatchers("/", "/index.html", "config.js", "/assets/**").permitAll()
                                         .anyRequest()
                                         .authenticated()
                 )
