d1bc00f7ea9a53fa3f11735545df75b311c8dad3
GANZO9055 <kosta.maslihov@gmail.com>
2026-02-20 21:43:31 +0300

    refactor(auth): delete SessionConfig

--- src/main/java/com/example/cloud_storage/CloudStorageApplication.java
+++ src/main/java/com/example/cloud_storage/CloudStorageApplication.java
@@ -2,8 +2,10 @@ package com.example.cloud_storage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 600)
 public class CloudStorageApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/cloud_storage/user/config/SessionConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.cloud_storage.user.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
-
-@Configuration
-@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 600)
-public class SessionConfig {
-}
