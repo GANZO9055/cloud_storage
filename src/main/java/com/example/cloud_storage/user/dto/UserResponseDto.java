75f106607691ba5fd5883d761767da0ce385afc7
GANZO9055 <kosta.maslihov@gmail.com>
2026-02-20 21:31:51 +0300

    refactor(auth): add validate UserResponseDto

--- src/main/java/com/example/cloud_storage/user/dto/UserResponseDto.java
+++ src/main/java/com/example/cloud_storage/user/dto/UserResponseDto.java
@@ -1,6 +1,8 @@
 package com.example.cloud_storage.user.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,5 +13,7 @@ import lombok.Setter;
 @Schema(description = "Ответ пользователя")
 public class UserResponseDto {
     @Schema(description = "Имя пользователя", example = "user_1")
+    @NotBlank(message = "Username cannot be null")
+    @Size(max = 50)
     private String username;
 }
