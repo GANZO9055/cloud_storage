4c026d677ecf16801928afd9e00ebbef5b28f6b4
GANZO9055 <kosta.maslihov@gmail.com>
2026-02-21 17:33:39 +0300

    refactor(minio): update FileResponseDto and FolderResponseDto

--- src/main/java/com/example/cloud_storage/minio/dto/directory/FolderResponseDto.java
+++ src/main/java/com/example/cloud_storage/minio/dto/directory/FolderResponseDto.java
@@ -3,6 +3,10 @@ package com.example.cloud_storage.minio.dto.directory;
 import com.example.cloud_storage.minio.dto.Resource;
 import com.example.cloud_storage.minio.dto.Type;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,9 +16,16 @@ import lombok.Setter;
 @Setter
 public class FolderResponseDto implements Resource {
     @Schema(description = "Путь до папки", example = "test1/test2/")
+    @Size(max = 500)
+    @NotBlank
     private String path;
+
     @Schema(description = "Имя папки", example = "test3/")
+    @Size(max = 100)
+    @NotBlank
     private String name;
+
     @Schema(description = "Тип ресурса", example = "FILE")
+    @NotNull
     private Type type;
 }
--- src/main/java/com/example/cloud_storage/minio/dto/file/FileResponseDto.java
+++ src/main/java/com/example/cloud_storage/minio/dto/file/FileResponseDto.java
@@ -3,6 +3,10 @@ package com.example.cloud_storage.minio.dto.file;
 import com.example.cloud_storage.minio.dto.Resource;
 import com.example.cloud_storage.minio.dto.Type;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,11 +17,20 @@ import lombok.Setter;
 @Schema(description = "Файл")
 public class FileResponseDto implements Resource {
     @Schema(description = "Путь до файла", example = "test1/test2/")
+    @Size(max = 500)
+    @NotBlank
     private String path;
+
     @Schema(description = "Имя файла", example = "test3.txt")
+    @Size(max = 100)
+    @NotBlank
     private String name;
+
     @Schema(description = "Размер файла", example = "42")
+    @Min(0)
     private Long size;
+
     @Schema(description = "Тип ресурса", example = "FILE")
+    @NotNull
     private Type type;
 }
