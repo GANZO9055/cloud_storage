bfb327d52440e346ada80b9d23b9ccf480b146eb
GANZO9055 <kosta.maslihov@gmail.com>
2026-02-22 20:03:30 +0300

    refactor(minio): update ResourceMapper

--- src/main/java/com/example/cloud_storage/minio/mapper/ResourceMapper.java
+++ src/main/java/com/example/cloud_storage/minio/mapper/ResourceMapper.java
@@ -1,7 +1,9 @@
 package com.example.cloud_storage.minio.mapper;
 
+import com.example.cloud_storage.minio.dto.Type;
 import com.example.cloud_storage.minio.dto.directory.FolderResponseDto;
 import com.example.cloud_storage.minio.dto.file.FileResponseDto;
+import com.example.cloud_storage.minio.model.FileMetadata;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -13,4 +15,6 @@ public interface ResourceMapper {
 
     @Mapping(target = "type", constant = "DIRECTORY")
     FolderResponseDto toFolder(String path, String name);
+
+    FileMetadata toFileMetadata(Integer userId, String path, String name, Long size, Type type);
 }
